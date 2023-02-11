7eddfe5831f8b5d1ebc68a12d5e69a476336a779
Rishi <rishipradhan896@gmail.com>
2023-02-11 15:23:56 +0530

    update according to the file name

--- LinearSearch.js
+++ LinearSearch.js
@@ -1,15 +1,27 @@
+
 function linearSearch(array, target) {
-    let result = []
-
-    for (let index = 0; index < array.length; index++) {
-        if (array[index] === target) {
-            result.push(index)
-        }
+    let result = [];
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] === target) {
+        result.push(i);
+      }
+   
     }
-    
-    return result
-}
-
-const res =  linearSearch([12, 34, 23, 7, 23, 23, 23, 23,4], 23)
-
-console.log(res)
+  
+  
+    return -1;
+  }
+  
+  // console.log(linearSearch([1, 5, 3, 4, 7, 5, 6], 5));
+  
+  (function linearSearch2() {
+    let target = 23;
+    let array = [ 45, 556, 67, 756, 23, 23];
+    let found = linearSearch(array, target);
+    if (found === -1) {
+      console.log("Target not found in the arr");
+    } else {
+      console.log(`Target ${target} found in the arr at index ${found}`);
+    }
+  })();
+  
