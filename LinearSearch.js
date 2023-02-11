72b0ae1f1220e15163c0ff7b6f1084030d593d3c
Rishi <rishipradhan896@gmail.com>
2023-02-11 15:17:24 +0530

    LinearSearch for multi index

--- LinearSearch.js
+++ LinearSearch.js
@@ -1,1 +1,15 @@
-function 
+function linearSearch(array, target) {
+    let result = []
+
+    for (let index = 0; index < array.length; index++) {
+        if (array[index] === target) {
+            result.push(index)
+        }
+    }
+    
+    return result
+}
+
+const res =  linearSearch([12, 34, 23, 7, 23, 23, 23, 23,4], 23)
+
+console.log(res)
